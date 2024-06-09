cc802ca39f9cfa3bb058c7b8d42801dfcaefa40a
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-06-09 13:55:37 +0300

    Update auth.py

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -79,7 +79,7 @@ def load_logged_in_user():
 @bp.route('/logout')
 def logout():
     session.clear()
-    return redirect(url_for('index'))
+    return redirect(url_for('base'))
 
 
 def login_required(view):
