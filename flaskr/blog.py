099c104cb5511aeba23dc2a79a5fa1db6faa18f4
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-06-09 13:39:29 +0300

    Update blog.py

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,3 +1,29 @@
+import os
+from flask import Flask
+from . import db
+from . import auth, blog
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    db.init_app(app)
+
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
+    return app
+
+
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for, jsonify
 )
