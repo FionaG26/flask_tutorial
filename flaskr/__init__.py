f3aed61844d84f11d6530c5b0eff4a2a5fb5b833
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-06-10 11:21:37 +0300

    Update __init__.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,18 +1,12 @@
 import os
 from flask import Flask
 
-
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
-    
-    # Configuration for serving static files
-    app.config['STATIC_FOLDER'] = 'static'
-    app.config['STATIC_URL_PATH'] = '/static'
-
 
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
@@ -24,18 +18,14 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
     from . import db
     db.init_app(app)
 
     from . import auth
     app.register_blueprint(auth.bp)
 
-    from . import blog
-    app.register_blueprint(blog.bp)
+    from .blog import bp
+    app.register_blueprint(bp)
     app.add_url_rule('/', endpoint='index')
 
     return app
