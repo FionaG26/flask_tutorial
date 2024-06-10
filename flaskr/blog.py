514597efb934a8e0f1f25cf73818b86a8d6693c8
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-06-10 13:54:01 +0300

    Update blog.py

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -88,6 +88,7 @@ def create():
             db.commit()
             article_id = db.execute('SELECT last_insert_rowid()').fetchone()[0]
             return redirect(url_for('blog.article', article_id=article_id))
+return render_template('blog/index.html')
 
 
 @bp.route('/autosave', methods=['POST'])
