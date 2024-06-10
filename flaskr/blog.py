70b8e138b019badf8cabf2edae0bbe45b505d1c4
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-06-10 12:23:17 +0300

    Update blog.py

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,12 +1,13 @@
 import os
+import datetime
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for, jsonify
 )
 from werkzeug.exceptions import abort
+from werkzeug.utils import secure_filename
 from flaskr.auth import login_required
 from flaskr.db import get_db
 import json
-import datetime
 
 bp = Blueprint('blog', __name__, template_folder='templates')
 
@@ -47,7 +48,6 @@ def create():
 
         if publish_date:
             try:
-                # Validate the publish_date format
                 datetime.datetime.strptime(publish_date, '%Y-%m-%d %H:%M:%S')
             except ValueError:
                 error = 'Invalid date format for publish date. Use YYYY-MM-DD HH:MM:SS.'
@@ -61,7 +61,6 @@ def create():
                 if not os.path.exists(uploads_dir):
                     os.makedirs(uploads_dir)
 
-                # Use secure_filename to prevent directory traversal attacks
                 filename = secure_filename(image.filename)
                 image_path = os.path.join(uploads_dir, filename)
                 image.save(image_path)
@@ -75,7 +74,7 @@ def create():
             )
             db.commit()
             article_id = db.execute('SELECT last_insert_rowid()').fetchone()[0]
-            return redirect(url_for('blog.article', id=article_id))
+            return redirect(url_for('blog.article', article_id=article_id))
     return render_template('blog/create.html')
 
 @bp.route('/autosave', methods=['POST'])
@@ -128,34 +127,55 @@ def update(id):
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
+        summary = request.form.get('summary')
+        image = request.files.get('image')
+        category = request.form.get('category')
+        tags = request.form.get('tags')
+        publish_date = request.form.get('publish_date')
+        seo_title = request.form.get('seo_title')
+        seo_description = request.form.get('seo_description')
+        seo_keywords = request.form.get('seo_keywords')
         error = None
 
         if not title:
             error = 'Title is required.'
+        if not body:
+            error = 'Body is required.'
+
+        if publish_date:
+            try:
+                datetime.datetime.strptime(publish_date, '%Y-%m-%d %H:%M:%S')
+            except ValueError:
+                error = 'Invalid date format for publish date. Use YYYY-MM-DD HH:MM:SS.'
 
         if error is not None:
             flash(error)
         else:
+            image_url = post['image']
+            if image and image.filename != '':
+                uploads_dir = os.path.join(bp.root_path, 'static/uploads')
+                if not os.path.exists(uploads_dir):
+                    os.makedirs(uploads_dir)
+
+                filename = secure_filename(image.filename)
+                image_path = os.path.join(uploads_dir, filename)
+                image.save(image_path)
+                image_url = f'uploads/{filename}'
+
             db = get_db()
             db.execute(
-                'UPDATE post SET title = ?, body = ? '
+                'UPDATE post SET title = ?, body = ?, summary = ?, image = ?, category = ?, tags = ?, publish_date = ?, seo_title = ?, seo_description = ?, seo_keywords = ?'
                 'WHERE id = ?',
-                (title, body, id)
+                (title, body, summary, image_url, category, tags, publish_date, seo_title, seo_description, seo_keywords, id)
             )
             db.commit()
-            return redirect(url_for('blog.index'))
-
+            return redirect(url_for('blog.article', article_id=id))
     return render_template('blog/update.html', post=post)
-
-@bp.route('/<int:id>', methods=('GET',))
-def view(id):
-    post = get_post(id)
-    return render_template('blog/view.html', post=post)
 
 @bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
 def delete(id):
-    get_post(id)
+    post = get_post(id)
     db = get_db()
     db.execute('DELETE FROM post WHERE id = ?', (id,))
     db.commit()
